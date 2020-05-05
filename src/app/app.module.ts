9d3a48da8d1404a7522e826174e284be3cb6ace7
weiguo.ni <neefrankie@gmail.com>
2020-05-05 11:54:19 +0800

    layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavComponent } from './layout/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
