0ac143f7c31ee34ec047eb4189cbde69103ee98a
weiguo.ni <neefrankie@gmail.com>
2020-05-06 14:58:45 +0800

    feat: type definition

---
+++ src/app/data/schema/admin.ts
@@ -0,0 +1,18 @@
+export interface Credentials {
+  email: string;
+  password: string;
+}
+
+export interface AdminAccount {
+  id: string;
+  email: string;
+  displayName: string | null;
+  active: boolean;
+  verified: boolean;
+}
+
+export interface Passport extends AdminAccount {
+  teamId: string | null;
+  expiresAt: number;
+  token: string;
+}
