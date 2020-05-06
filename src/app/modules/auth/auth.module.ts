adbcba39d1984721fef3c6109a453086648122a3
weiguo.ni <neefrankie@gmail.com>
2020-05-06 17:29:59 +0800

    feat: login form

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { SignupComponent } from './signup/signup.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
   ],
 })
 export class AuthModule {}
