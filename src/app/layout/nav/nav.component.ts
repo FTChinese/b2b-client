d40cfe0a7daf8023e18fb06928b910cdd4220d14
weiguo.ni <neefrankie@gmail.com>
2020-05-06 15:00:54 +0800

    feat: nav component

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from 'src/app/data/schema/link';
 
 @Component({
   selector: 'app-nav',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+
+  navItems: Link[] = [
+    {
+      name: '许可',
+      href: '/licences'
+    },
+    {
+      name: '成员',
+      href: '/users',
+    },
+    {
+      name: '邀请',
+      href: '/invitations',
+    },
+    {
+      name: '交易历史',
+      href: '/transactions',
+    },
+    {
+      name: '设置',
+      href: '/settings'
+    },
+    {
+      name: '退出',
+      href: '/logout'
+    },
+  ];
 
   constructor() { }
 
