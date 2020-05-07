16d8900f9352dc9255d6ca0bdca57fb55011ff7f
weiguo.ni <neefrankie@gmail.com>
2020-05-07 16:16:47 +0800

    feat: define form data

---
+++ src/app/data/schema/form-data.ts
@@ -0,0 +1,29 @@
+// Forgot password form.
+export interface Email {
+  email: string;
+}
+
+interface Password {
+  password: string;
+}
+
+interface ConfirmPassword {
+  confirmPassword: string;
+}
+// This is used for:
+// Login form data;
+// API login request body;
+// API signup request body.
+export type Credentials = Email & Password;
+
+// Sign-up form. Turn to Credentials when submitting to API.
+export type SignUp = Email & Password & ConfirmPassword;
+
+// Password resetting form.
+export type PwResetForm = Password & ConfirmPassword;
+
+// Resetting password request body.
+export type ResettingPassword = Password & {
+  token: string;
+};
+
