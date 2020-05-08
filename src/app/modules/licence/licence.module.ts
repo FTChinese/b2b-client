b20b39c12db62d4d4bf3486927782d5bc5bed1c1
weiguo.ni <neefrankie@gmail.com>
2020-05-08 17:10:40 +0800

    feat: add LicenceListComponent

--- src/app/modules/licence/licence.module.ts
+++ src/app/modules/licence/licence.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LicenceRoutingModule } from './licence-routing.module';
+import { LicenceListComponent } from './licence-list/licence-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LicenceListComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
     LicenceRoutingModule
   ]
 })
