c1c6b6d9fd1263a83b3b5fc38951ab5d2d45db24
weiguo.ni <neefrankie@gmail.com>
2020-05-08 15:53:04 +0800

    feat: define product

---
+++ src/app/data/schema/product.ts
@@ -0,0 +1,23 @@
+import { Tier, Cycle } from './enum';
+
+export interface Discount {
+  threshold: number;
+  priceOff: number;
+}
+
+export interface Plan {
+  id: string;
+  price: number;
+  tier: Tier;
+  cycle: Cycle;
+  discounts: Discount[];
+}
+
+export interface Product {
+  id: string;
+  tier: Tier;
+  heading: string;
+  smallPrint: string | null;
+  description: string[];
+  plan: Plan;
+}
