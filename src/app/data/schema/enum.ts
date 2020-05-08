2f4dd406b34adedb2462d81b20d22cbe642a8eee
weiguo.ni <neefrankie@gmail.com>
2020-05-08 17:29:01 +0800

    fix: typo

--- src/app/data/schema/enum.ts
+++ src/app/data/schema/enum.ts
@@ -7,4 +7,4 @@ export type Platform = 'web' | 'ios' | 'android'
 export type SubStatus = 'active' | 'canceled' | 'incomplete' | 'incomplate_expired' | 'past_due' | 'trialing' | 'unpaid';
 export type OrderType = 'create' | 'renew' | 'upgrade';
 export type AccountKind = 'ftc' | 'wechat' | 'linked';
-export type LicenceStatus = 'available' | 'inviated' | 'granted';
+export type LicenceStatus = 'available' | 'invited' | 'granted';
