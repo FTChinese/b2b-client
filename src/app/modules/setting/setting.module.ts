e4e05b4f4faf7ce1e3f0eb69220bd5cc7039d549
weiguo.ni <neefrankie@gmail.com>
2020-05-08 17:11:30 +0800

    feat: settings form

--- src/app/modules/setting/setting.module.ts
+++ src/app/modules/setting/setting.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings/settings.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [SettingsComponent]
 })
