d6e044b61d2aa7896b27b1c0f07f20ff14b2284b
weiguo.ni <neefrankie@gmail.com>
2020-05-08 15:51:19 +0800

    feat: team guard

---
+++ src/app/core/guard/team.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.authService.isTeamSet) {
+        this.router.navigate(['/products']);
+        return false;
+      }
+
+      return true;
+  }
+}
