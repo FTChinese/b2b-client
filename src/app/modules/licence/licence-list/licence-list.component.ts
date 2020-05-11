237bd8a1299ed9f5f2cc3b16644268824446c1e8
weiguo.ni <neefrankie@gmail.com>
2020-05-11 10:26:10 +0800

    show licences

--- src/app/modules/licence/licence-list/licence-list.component.ts
+++ src/app/modules/licence/licence-list/licence-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Licence } from 'src/app/data/schema/licence';
-import { licenceID, stdPlan, invitationID, randomString } from 'src/app/data/mock';
+import { licences } from 'src/app/data/mock';
 
 @Component({
   selector: 'app-licence-list',
@@ -9,67 +8,7 @@ import { licenceID, stdPlan, invitationID, randomString } from 'src/app/data/moc
 })
 export class LicenceListComponent implements OnInit {
 
-  private createdUtc = '2020-05-05T17:19:00Z';
-
-  licences: Licence[] = [
-    {
-      id: licenceID(),
-      teamId: '',
-      expireDate: '2021-05-20',
-      trialStart: '2020-05-05',
-      trialEnd: '2020-05-12',
-      status: 'available',
-      createdUtc: this.createdUtc,
-      updatedUtc: this.createdUtc,
-      lastInvitationId: null,
-      lastInviteeEmail: null,
-      plan: stdPlan,
-      assignee: {
-        ftcId: null,
-        email: null,
-        userName: null,
-        isVip: false,
-      },
-    },
-    {
-      id: licenceID(),
-      teamId: '',
-      expireDate: '2021-05-20',
-      trialStart: '2020-05-05',
-      trialEnd: '2020-05-12',
-      status: 'invited',
-      createdUtc: this.createdUtc,
-      updatedUtc: this.createdUtc,
-      lastInvitationId: invitationID(),
-      lastInviteeEmail: 'testA@example.rog',
-      plan: stdPlan,
-      assignee: {
-        ftcId: null,
-        email: null,
-        userName: null,
-        isVip: false,
-      },
-    },
-    {
-      id: licenceID(),
-      teamId: '',
-      expireDate: '2021-05-20',
-      trialStart: '2020-05-05',
-      trialEnd: '2020-05-12',
-      status: 'granted',
-      createdUtc: this.createdUtc,
-      updatedUtc: this.createdUtc,
-      lastInvitationId: invitationID(),
-      lastInviteeEmail: 'testB@example.org',
-      plan: stdPlan,
-      assignee: {
-        ftcId: randomString(),
-        email: 'testB@example.org',
-        userName: 'test user b',
-        isVip: false,
-      },
-    }
-  ];
+  readonly licences = licences;
 
   constructor() { }
 
