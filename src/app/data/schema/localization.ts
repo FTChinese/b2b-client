103369fab113f8fe681430074ca2c4323e71e7f6
weiguo.ni <neefrankie@gmail.com>
2020-05-15 11:12:05 +0800

    feat: localization for chinese

---
+++ src/app/data/schema/localization.ts
@@ -0,0 +1,11 @@
+import { Tier, Cycle } from './enum';
+
+export const tiers: Record <Tier, string> = {
+  standard: '标准版',
+  premium: '高端版'
+};
+
+export const cycles: Record<Cycle, string> = {
+  month: '月',
+  year: '年'
+};
