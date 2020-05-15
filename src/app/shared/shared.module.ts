f461942dfaf04e512608ae6edd6a4c978acc5963
weiguo.ni <neefrankie@gmail.com>
2020-05-15 15:31:15 +0800

    refactor: removed button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicControlComponent } from './dynamic-control/dynamic-control.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { ButtonComponent } from './button/button.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EditionPipe } from './pipes/edition.pipe';
 import { CyclePipe } from './pipes/cycle.pipe';
+import { ButtonDirective } from './directive/button.directive';
 
 @NgModule({
   declarations: [
     DynamicControlComponent,
     DynamicFormComponent,
-    ButtonComponent,
     EditionPipe,
     CyclePipe,
+    ButtonDirective,
   ],
   imports: [
     CommonModule,
@@ -22,7 +22,6 @@ import { CyclePipe } from './pipes/cycle.pipe';
   exports: [
     DynamicControlComponent,
     DynamicFormComponent,
-    ButtonComponent,
     EditionPipe,
     CyclePipe,
   ]
