c412fa887cd6ec8e953095bf9bab822abd8ef4e3
weiguo.ni <neefrankie@gmail.com>
2020-05-27 12:12:57 +0800

    refactor: uppercase

--- cmd/index.go
+++ cmd/index.go
@@ -1,6 +1,8 @@
+package controller
+
 // This file is generated from the front end project b2b-client.
 // Do not touch it.
 
-// B2BIndexHtml is the index file for angular web app.
-const B2BIndexHtml = `
+// B2BIndexHTML is the index file for angular web app.
+const B2BIndexHTML = `
 {{content}}`
