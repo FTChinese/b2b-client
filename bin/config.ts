2071070728c64cb6cb76355ec8916f7b0c538c9a
weiguo.ni <neefrankie@gmail.com>
2020-07-21 14:21:35 +0800

    refactor: directory names

--- bin/config.ts
+++ bin/config.ts
@@ -1,26 +1,28 @@
 import { resolve } from 'path';
 
-const projectName = 'superyard';
-
 const isProd = process.env.NODE_ENV === 'production';
 
 console.log('NODE_ENV: %s', process.env.NODE_ENV);
 
-const staticPrefix = isProd
-  ? `/static/${projectName}/`
-  : '/static/';
+const serverProjectName = 'ftacademy';
+const clientProjectName = 'b2b';
 
+// The URL prefix used for link and script tag.
+const staticPrefix = `/static/${clientProjectName}/`;
+
+// Copy js and css to build directory of the server project for development,
+// or to dedicated static server directory.
 const jsCssCopyTarget = isProd
-  ? `../ft-interact/${projectName}`
-  : `../${projectName}/build/public/static`;
+  ? `../ftac/frontend/${clientProjectName}`
+  : `../${serverProjectName}/build/public/static/${clientProjectName}`;
 
 const config = {
   production: isProd,
   staticPrefix,
-  goViewFile: resolve(process.cwd(), 'dist/templates.go'),
-  goTemplate: 'views.go.njk',
-  htmlCopyTarget: `../${projectName}/web/views`,
-  jsCssCopyTarget,
+  goTemplate: 'views.go.njk', // The nunjucks template used to generated go template file
+  goOutFile: resolve(process.cwd(), 'dist/templates.go'), // The generated go file containing inlined templates.
+  htmlCopyTarget: `../${serverProjectName}/web/views`, // The directory used to place generated go tempalate file.
+  jsCssCopyTarget, // The directory used to put js and css file.
 };
 
 export default config;
