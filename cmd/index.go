1e97e3d520ad34381d5992745c22e04176b40377
weiguo.ni <neefrankie@gmail.com>
2020-07-21 15:26:29 +0800

    removed cmd dir

--- cmd/index.go
+++
@@ -1,8 +0,0 @@
-package controller
-
-// This file is generated from the front end project b2b-client.
-// Do not touch it.
-
-// B2BIndexHTML is the index file for angular web app.
-const B2BIndexHTML = `
-{{content}}`
