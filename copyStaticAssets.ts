93788f58e8704f3ae1c9ab05ae2d5fcb326ca484
weiguo.ni <neefrankie@gmail.com>
2020-07-21 14:36:40 +0800

    refactor: cli to copy static assets

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -2,15 +2,17 @@ import * as shell from 'shelljs';
 
 import config from './bin/config';
 
+const currentProjectName = 'b2b-client';
+
 shell.mkdir('-p', config.htmlCopyTarget);
 shell.mkdir('-p', config.jsCssCopyTarget);
 
-shell.cp('dist/superyard-client/*.js', config.jsCssCopyTarget);
+shell.cp(`dist/${currentProjectName}/*.js`, config.jsCssCopyTarget);
 
 if (config.production) {
-  shell.cp('dist/superyard-client/*.css', config.jsCssCopyTarget);
+  shell.cp(`dist/${currentProjectName}/*.css`, config.jsCssCopyTarget);
 } else {
-  shell.cp('dist/superyard-client/*.map', config.jsCssCopyTarget);
+  shell.cp(`dist/${currentProjectName}/*.map`, config.jsCssCopyTarget);
 }
 
-shell.cp(config.goViewFile, config.htmlCopyTarget);
+shell.cp(config.goOutFile, config.htmlCopyTarget);
