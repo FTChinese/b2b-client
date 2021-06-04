2341969a3a1e75f20d1c8e384fc56c5150039083
weiguo.ni <neefrankie@gmail.com>
2021-06-04 19:19:35 +0800

    refactor: licence

--- src/app/data/schema/licence.ts
+++ src/app/data/schema/licence.ts
@@ -1,30 +1,31 @@
 import { LicenceStatus } from './enum';
-import { Plan } from './product';
+import { Edition, Price } from './product';
 import { Assignee } from './assignee';
 import { parseISO, isBefore } from 'date-fns';
+import { Invitation } from './invitation';
 
 // All licence should have a Renw button;
 // Only standard version has Upgrade button;
-export interface Licence {
+export interface Licence extends Edition {
   id: string;
   teamId: string;
-  expireDate: string | null;
-  trialStart: string | null;
-  trialEnd: string | null;
+  periodStartUtc: string;
+  periodEndUtc: string;
+  trialStartUtc: string | null;
+  trialEndUtc: string | null;
   // available: admin could send invitation
   // invited: admin could revoke the invitation
   // granted: admin could revoke the licence
   status: LicenceStatus | null;
   createdUtc: string | null;
   updatedUtc: string | null;
-  lastInvitationId: string | null;
-  lastInviteeEmail: string | null;
-  plan: Plan;
+  lastInvitation: Invitation;
+  lastPrice: Price;
   assignee: Assignee;
 }
 
 export function isExpired(l: Licence): boolean {
-  return isBefore(parseISO(l.expireDate), new Date());
+  return isBefore(parseISO(l.periodEndUtc), new Date());
 }
 
 export interface LicenceList {
